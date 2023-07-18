4c86335505d1551e8f5a10b768a81a5a8f88d37b
Rajni bala <balatamna16@gmail.com>
2023-07-18 14:41:49 -0400

    find all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,21 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+    // be sure to include its associated Products
+  try{
+    const categoriesData = await Category.findAll({
+      include: [
+        {model:Product}
+      ],
+    });
+    res.status(200).json(categoriesData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.get('/:id', (req, res) => {
