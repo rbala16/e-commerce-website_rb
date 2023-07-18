c02d509ee948f32c674b9334dd93c142c8ec9c64
Rajni bala <balatamna16@gmail.com>
2023-07-18 15:06:34 -0400

    delete category by its id value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -60,7 +60,7 @@ router.put('/:id',async (req, res) => {
         id: req.params.id,
       }
     });
-    if (!categoriesData[0]) {
+    if (!categoriesData) {
       res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
@@ -70,8 +70,22 @@ router.put('/:id',async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',async (req, res) => {
   // delete a category by its `id` value
+  try{
+  const categoriesData= await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!categoriesData) {
+    res.status(404).json({ message: 'No category found with that id!' });
+    return;
+  }
+  res.status(200).json(categoriesData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 module.exports = router;
