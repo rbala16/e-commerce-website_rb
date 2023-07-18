ddbf720b6f4094788268a34897cc9666522e49cf
Rajni bala <balatamna16@gmail.com>
2023-07-18 14:46:32 -0400

    find one category by its id value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,9 +20,26 @@ router.get('/',async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categoriesData = await Category.findByPk(req.params.id,{
+      include: [
+        {model:Product}
+      ],
+    });
+
+    if (!categoriesDataData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoriesData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  } 
 });
 
 router.post('/', (req, res) => {
