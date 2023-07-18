ca81442e345be5603c5f700e98c04261b2db6b99
Rajni bala <balatamna16@gmail.com>
2023-07-18 14:53:06 -0400

    create new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,7 +30,7 @@ router.get('/:id',async (req, res) => {
       ],
     });
 
-    if (!categoriesDataData) {
+    if (!categoriesData) {
       res.status(404).json({ message: 'No Category found with that id!' });
       return;
     }
@@ -42,8 +42,14 @@ router.get('/:id',async (req, res) => {
   } 
 });
 
-router.post('/', (req, res) => {
+router.post('/',async (req, res) => {
   // create a new category
+  try {
+    const categoriesData = await Category.create(req.body);
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
