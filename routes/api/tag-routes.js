f4876605fee5e7a105b8e0bb2328cf30e18d0a95
Rajni bala <balatamna16@gmail.com>
2023-07-18 18:09:55 -0400

    get single tag by its id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,9 +19,26 @@ router.get('/',async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const tagRoutesData = await Tag.findByPk(req.params.id,{
+      include: [
+        {model:Product}
+      ],
+    });
+
+    if (!tagRoutesData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagRoutesData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  } 
 });
 
 router.post('/', (req, res) => {
